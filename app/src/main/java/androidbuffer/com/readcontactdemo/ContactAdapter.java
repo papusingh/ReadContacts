6f3a83c02e048f0f2c635ed267f51236f210b856
papu singh <papu.singh@incred.com>
2018-08-21 11:53:16 +0530

    1st push in read contact

---
+++ app/src/main/java/androidbuffer/com/readcontactdemo/ContactAdapter.java
@@ -0,0 +1,63 @@
+package androidbuffer.com.readcontactdemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by incred-dev on 6/7/18.
+ */
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MyViewHolder>{
+
+    private List<ContactModel> contactModelList;
+
+    public ContactAdapter(List<ContactModel> contactModelList) {
+        this.contactModelList = contactModelList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        ContactModel model = contactModelList.get(position);
+        if (model != null){
+            if (model.getName() != null){
+                holder.name.setText(model.getName());
+            }
+
+            if (model.getNumber() != null){
+                StringBuffer buffer = new StringBuffer();
+                for (String number:model.getNumber()){
+                    buffer.append(number).append("\n");
+                }
+                holder.number.setText(buffer);
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactModelList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name, number;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.tvName);
+            number = itemView.findViewById(R.id.tvNumber);
+        }
+    }
+}
---
+++ app/src/main/java/androidbuffer/com/readcontactdemo/MainActivity.java
@@ -0,0 +1,121 @@
+package androidbuffer.com.readcontactdemo;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    private List<ContactModel> contactModelList = new ArrayList<>();
+    ContactAdapter contactAdapter;
+    private static String[] PERMISSION_CONTACT = {Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS};
+    private static final int REQUEST_CONTACT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv);
+        setDataToAdapter();
+        requestContactsPermissions();
+    }
+
+    private void setDataToAdapter(){
+        contactAdapter = new ContactAdapter(contactModelList);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(contactAdapter);
+    }
+
+    private void getContactInfo(){
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PHONE_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String PHONE_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(CONTENT_URI,null,null,null,DISPLAY_NAME);
+
+        if (cursor.getCount() > 0){
+            while (cursor.moveToNext()){
+                String CONTACT_ID = cursor.getString(cursor.getColumnIndex(ID));
+                String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
+
+                int hasPhoneNumber = cursor.getInt(cursor.getColumnIndex(HAS_PHONE_NUMBER));
+                ContactModel contactModel = new ContactModel();
+                if (hasPhoneNumber > 0){
+                    contactModel.setName(name);
+
+                    Cursor phoneCursor = contentResolver.query(PHONE_URI, new String[]{NUMBER},PHONE_ID+" = ?",new String[]{CONTACT_ID},null);
+                    List<String> contactList = new ArrayList<>();
+                    phoneCursor.moveToFirst();
+                    while (!phoneCursor.isAfterLast()){
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER)).replace(" ","");
+                        contactList.add(phoneNumber);
+                        phoneCursor.moveToNext();
+                    }
+                    contactModel.setNumber(contactList);
+                    contactModelList.add(contactModel);
+                    phoneCursor.close();
+                }
+            }
+            contactAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public void requestContactsPermissions(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_CONTACTS) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_CONTACTS)){
+
+                Snackbar.make(recyclerView, "permission Contact", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ActivityCompat.requestPermissions(MainActivity.this,PERMISSION_CONTACT,REQUEST_CONTACT);
+                            }
+                        }).show();
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,PERMISSION_CONTACT,REQUEST_CONTACT);
+            }
+        } else {
+            getContactInfo();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int result : grantResults){
+            if (result == PackageManager.PERMISSION_GRANTED){
+                getContactInfo();
+            }
+        }
+    }
+}
